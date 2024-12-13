14e50beafce8968e144073ecbaa6565c108657b3
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-12-13 22:19:51 +0530

    todo controllers

--- NestJs/todo/backend/src/todos/todos.controller.ts
+++ NestJs/todo/backend/src/todos/todos.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { TodosService } from './todos.service';
+import { CreateTodoDto } from './dto/createTodo.dto';
 
 @Controller('todos')
-export class TodosController {}
+export class TodosController {
+    constructor(private readonly todosService: TodosService) { }
+
+    @Post()
+    createTodo(@Body() todoData: CreateTodoDto) {
+        return this.todosService.createTodo(todoData)
+    }
+
+    @Get()
+    findAllTodos() {
+        return this.todosService.getTodos()
+    }
+}
